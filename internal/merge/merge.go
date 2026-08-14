1e29e29d31447cf0ad54a1c39501c84994872cea
liuyonghui <liuyonghui@example.com>
2026-08-15 02:47:50 +0800

    修复：环境变量覆盖优先级弄反

--- internal/merge/merge.go
+++ internal/merge/merge.go
@@ -3,13 +3,13 @@ package merge
 import "github.com/LYH2263/go-config-layer/internal/model"
 
 
-// Merge：本意 env 覆盖 file，实现写反了。
+// Merge：env 覆盖 file。
 func Merge(file, env model.Map) model.Map {
 	out := model.Map{}
-	for k, v := range env {
+	for k, v := range file {
 		out[k] = v
 	}
-	for k, v := range file {
+	for k, v := range env {
 		out[k] = v
 	}
 	return out
